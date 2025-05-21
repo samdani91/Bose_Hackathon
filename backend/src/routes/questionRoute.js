1a6975a135d8e7877e3a5047663c526a4f771e0e
kibria30 <kibria8007@gmail.com>
2025-05-21 19:03:46 +0600

    question details route is fixed

--- backend/src/routes/questionRoute.js
+++ backend/src/routes/questionRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createQuestion, deleteQuestion, updateQuestion } from "../controllers/questionController.js";
+import { createQuestion, deleteQuestion, getQuestionById, updateQuestion } from "../controllers/questionController.js";
 const router = express.Router();
 
 
 router.post("/create", createQuestion);
 router.post("/update/:id", updateQuestion);
 router.delete("/delete/:id", deleteQuestion);
+router.get("/:id", getQuestionById);
 
 export default router;
