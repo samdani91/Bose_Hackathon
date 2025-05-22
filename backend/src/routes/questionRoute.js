52d81bdf3825b573cea7a88d7e56d31bf930a3b0
saimon4u <bsse1402@iit.du.ac.bd>
2025-05-22 21:56:57 +0600

    fix question fetching for all

--- backend/src/routes/questionRoute.js
+++ backend/src/routes/questionRoute.js
@@ -1,16 +1,17 @@
 import express from "express";
 import { generateQuiz } from "../controllers/quizController.js";
 import { createQuestion, deleteQuestion, getAllQuestions, getQuestionById, getQuestionsByUserId, updateQuestion, getQuestionsByTags } from "../controllers/questionController.js";
+import { authenticateToken } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 
-router.post("/create", createQuestion);
-router.post("/quiz", generateQuiz);
+router.post("/create", authenticateToken, createQuestion);
+router.post("/quiz", authenticateToken, generateQuiz);
 router.post("tags", getQuestionsByTags);
 router.get("/", getAllQuestions);
-router.post("/update/:id", updateQuestion);
-router.delete("/delete/:id", deleteQuestion);
+router.post("/update/:id",authenticateToken, updateQuestion);
+router.delete("/delete/:id",authenticateToken, deleteQuestion);
 router.get("/:id", getQuestionById);
-router.get("/user/:id",getQuestionsByUserId);
+router.get("/user/:id", authenticateToken, getQuestionsByUserId);
 
 export default router;
